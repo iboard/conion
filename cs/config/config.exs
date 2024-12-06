586893e6e3fb7528352e80480254ebaeaf9153de
Andreas Altendorfer <andreas@altendorfer.at>
2024-12-06 23:38:02 +0100

    Remove unused module Gateway.Server

--- cs/config/config.exs
+++ cs/config/config.exs
@@ -17,7 +17,7 @@ config :logger,
 
 config :cs,
   application_children: [
-    {CentralScrutinizer.Gateway.Server, name: CentralScrutinizer.Gateway.Server},
+    # {CentralScrutinizer.Gateway.Server, name: CentralScrutinizer.Gateway.Server},
     {CentralScrutinizer.Store.Server, name: CentralScrutinizer.Store.Server},
     {CentralScrutinizer.Store.BucketSupervisor, name: CentralScrutinizer.Store.BucketSupervisor}
   ]
--- cs/lib/central_scrutinizer/gateway/server.ex
+++
@@ -1,8 +0,0 @@
-defmodule CentralScrutinizer.Gateway.Server do
-  @moduledoc """
-  CentralScrutinizer's main process. Started from the application
-  Supervisor.
-  """
-
-  use CentralScrutinizer.CommonServer
-end
